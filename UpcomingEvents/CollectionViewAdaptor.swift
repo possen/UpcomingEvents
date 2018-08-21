434b20442ef9aa2325d030add647f2427e11473a
Paul Ossenbruggen <possen@gmail.com>
2018-08-20 21:44:35 -0700

    update

--- UpcomingEvents/CollectionViewAdaptor.swift
+++ UpcomingEvents/CollectionViewAdaptor.swift
@@ -8,54 +8,6 @@
 
 import UIKit
 
-protocol CollectionViewSectionAdaptorProtocol {
-    associatedtype HeaderCellType
-    associatedtype ModelType
-}
-
-// generic section adaptor to match model with header cell
-internal class CollectionViewSectionAdaptor<HeaderCell: UICollectionReusableView, Model>:
-        NSObject, CollectionViewSectionAdaptorProtocol {
-    typealias HeaderCellType = HeaderCell
-    typealias ModelType = Model
-    private let reuseIdentfier: String
-    private let configure: (HeaderCell, Model) -> Void
-    private var models: [Model]
-
-    internal init(
-        reuseIdentfier: String,
-        collectionView: UICollectionView,
-        configure: @escaping (HeaderCell, Model) -> Void
-    ) {
-        self.reuseIdentfier = reuseIdentfier
-        self.configure = configure
-        self.models = []
-    }
-
-    internal func change( sections: [Model]) {
-        self.models = sections
-    }
-
-    internal func cellForSectionHeader(
-        collectionView: UICollectionView,
-        kind: String,
-        indexPath: IndexPath
-    ) -> HeaderCell {
-
-        guard let header = collectionView.dequeueReusableSupplementaryView(
-            ofKind: kind,
-            withReuseIdentifier: "DayHeader",
-            for: indexPath
-        ) as? HeaderCell else {
-            fatalError("Could not create cell")
-        }
-
-        let model = models[indexPath.section]
-        configure(header, model)
-        return header
-    }
-}
-
 // generic adaptor to match model with cells
 internal class CollectionViewAdaptor<CollectionCell: UICollectionViewCell, Model>: NSObject,
         UICollectionViewDelegate, UICollectionViewDataSource {
